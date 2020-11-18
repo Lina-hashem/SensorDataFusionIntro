f8c1bbd9c26dd2b9de9ed069a91b02111a0e02c8
Lina-hashem <74468854+Lina-hashem@users.noreply.github.com>
2020-11-18 15:54:32 +0200

    Update MovingObject.py

--- MovingObject.py
+++ MovingObject.py
@@ -1,7 +1,6 @@
-
 import math
 import numpy as np
-import matplotlib.pyplot   as plt
+import matplotlib.pyplot as plt
 import numpy.linalg as la
 from mpl_toolkits import mplot3d
 
@@ -10,30 +9,27 @@ class MovingObject:
     deltaT = 1/60 #deltaT of one minute
     initial_position = np.array([10,1,1]) # a_0 = [10,1,1] column vector
     v = 20 
+    print(initial_position[0])
+    print(initial_position[1])
+
     time = np.arange(0,initial_position[0]/v,deltaT)
     
     position = np.array([initial_position[0]+ v*time,
                          initial_position[1]* np.sin((4*np.pi*v/initial_position[0]) * time),
                          np.sin((np.pi*v/initial_position[0]) * time)
                          ])
+    velocity = np.array([v*time, (np.cos((4*np.pi*v/initial_position[0])*time)*(4*np.pi*v/initial_position[0])),
+                             (np.cos((np.pi*v/initial_position[0])*time)*(np.pi*v/initial_position[0]))])
+    acc = -q* np.array([time,
+                    -1*(np.sin((4*np.pi*v/initial_position[0])*time)*(4*np.pi*v/initial_position[0])**2),
+                        -1*(np.sin((np.pi*v/initial_position[0])*time)*(np.pi*v/initial_position[0])**2)])
     
-    """
-    to be calculated by the taking the derivative:  (a)
-    vel = v * np.array([0.5*np.cos(w*periodInterval), np.cos(2*w*periodInterval)])
-    acc = -q * np.array([0.25*np.sin(w*periodInterval),np.sin(2*w*periodInterval)])
-    """
-    
-    
-    """
-    Well be implemented later to gather a vector state describing position, velocity and acceleration 
-    
-    def getStateX(self):
-        X = np.hstack((np.hstack((self.r.transpose(),self.vel.transpose())),self.acc.transpose()))
-        return X
-    """
+
     
 moving_object = MovingObject()
 position = moving_object.position
+vel = moving_object.velocity
+acc = moving_object.acc
 
 print(position)
 
@@ -50,6 +46,26 @@ ax.set_ylabel('y')
 ax.set_zlabel('z')
 #plt.tick_params(axis='both', which='major', labelsize=9)
 plt.legend(loc='lower right')
+
+fig2 = plt.figure()
+ax2 = plt.axes(projection='3d')
+ax2.plot(vel[1], vel[2], zs=20, zdir='z',color = 'red' , label='velocity in (x, y)')
+plt.title("Velocity")
+ax2.set_xlabel('y')
+ax2.set_ylabel('z')
+ax2.set_zlabel('x')
+# Data for a three-dimensional line
+
+
+
+fig3 = plt.figure()
+ax3 = plt.axes(projection='3d')
+ax3.plot(acc[1], acc[2], zs=0, zdir='z', label='acceleration in (x, y)')
+plt.title("Acceleration")
+ax3.set_xlabel('y')
+ax3.set_ylabel('z')
+ax3.set_zlabel('x')
+
 
 
 plt.show()
